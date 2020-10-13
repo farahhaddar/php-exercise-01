350c682c5f756060cd61300b2e474e2a73557fc4
farahhaddar <farah.haddar99@gmail.com>
2020-10-14 01:19:02 +0300

    Planidrome done

---
+++ Palindrome/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<form method="get" action="result.php">
+<b>Input your word: <b> <input type="text" name="name"> <br>
+<button type="submit" > submit</button>
+
+</form>
+
+</body>
+
+</html>
